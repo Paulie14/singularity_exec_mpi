762e43310ef4e2d3ae26ae7cf40fa0b50183b5bd
Paulie14 <pavelexner@gmail.com>
2022-08-11 12:18:25 +0200

    Update test with python script.

--- testing_script/test_script_job.sh
+++ testing_script/test_script_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -S /bin/bash
-#PBS -l select=4:ncpus=1:mem=4gb
+#PBS -l select=2:ncpus=2:mem=4gb
 #PBS -l place=scatter
 #PBS -l walltime=00:01:00
 #PBS -q charon_2h
@@ -11,8 +11,31 @@ set -x
 
 which python3
 
-# run from the repository directory
-cd "/auto/liberec3-tul/home/pavel_exner/workspace/singularity_tryout_3" || exit
+cd $PBS_O_WORKDIR || exit
 pwd
 
-python3 singularity_exec_mpi.py -i flow123d-geomop-master_8d5574fc2.sif -n 4 -m /usr/local/mpich_3.4.2/bin/mpiexec python_script_runner.sh
+# collect arguments:
+# singularity_exec_mpi script path
+SING_SCRIPT="../singularity_exec_mpi.py"
+# singularity SIF image path (preferably create in advance)
+SING_FLOW="../flow123d_geomop-gnu:2.0.0.sif"
+# container mpiexec path
+IMG_MPIEXEC="/usr/local/mpich_3.4.2/bin/mpiexec"
+# program and its arguments
+PROG="-n 4 python3 -m mpi4py $PBS_O_WORKDIR/script2.py"
+
+# main call
+python3 $SING_SCRIPT -i $SING_FLOW -m $IMG_MPIEXEC -- $PROG
+
+
+
+# example with python virtual environment
+# 1) create SIF image
+#   >> cd ..
+#   >> singularity build flow123d_geomop-gnu:2.0.0.sif docker://flow123d/geomop-gnu:2.0.0
+# 2) create virtual environment within container
+#   >> singularity shell flow123d_geomop-gnu:2.0.0.sif
+#   >> cd testing_script
+#   >> ./setup_python_environment.sh
+# 3) use auxiliary python_script_runner.sh to run the script in venv
+# PROG="-n 4 $PBS_O_WORKDIR/python_script_runner.sh"
